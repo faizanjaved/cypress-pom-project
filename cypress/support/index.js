e09db7a6f91d1f39e66d633a3a571c136a5bd277
Faizan Javed <fjaved@science37.com>
2020-11-07 22:51:48 +0500

    Display video/screenshot of failed tests in report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,17 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+import addContext from 'mochawesome/addContext';
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+const titleToFileName = (title) => title.replace(/[:\/]/g, '');
+
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === 'failed') {
+        const filename = `${titleToFileName(runnable.parent.title)} -- ${titleToFileName(test.title)} (failed).png`;
+        addContext({ test }, `../../screenshots/${Cypress.spec.name}/${filename}`);
+        addContext({ test }, `../../videos/${Cypress.spec.name}.mp4`);
+    }
+});
